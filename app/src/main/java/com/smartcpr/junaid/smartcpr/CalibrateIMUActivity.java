8614071d951967aa93504254300ed6d73019cba5
Junaid Siddiqui <junaidas97@gmail.com>
2018-08-27 15:16:50 -0400

    Calibration Activity

--- app/src/main/java/com/smartcpr/junaid/smartcpr/CalibrateIMUActivity.java
+++ app/src/main/java/com/smartcpr/junaid/smartcpr/CalibrateIMUActivity.java
@@ -2,82 +2,112 @@ package com.smartcpr.junaid.smartcpr;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.smartcpr.junaid.smartcpr.BluetoothData.BluetoothDataMainStream;
-import com.smartcpr.junaid.smartcpr.BluetoothData.BluetoothStream;
+import com.smartcpr.junaid.smartcpr.BluetoothData.BluetoothDeviceData;
+import com.smartcpr.junaid.smartcpr.BluetoothData.BluetoothDeviceManager;
 import com.smartcpr.junaid.smartcpr.BluetoothData.ManageData;
 import com.smartcpr.junaid.smartcpr.CalibrateIMUFragments.CalibratedIMUFragment;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalibrateIMUActivity extends AppCompatActivity {
 
     private final static String TAG = "CalibrateIMUActivity";
 
-    private BluetoothDataMainStream bluetoothStream;
+    private BluetoothDeviceManager bluetoothStream;
 
-    private InputStream mmInStream;
+    private ManageData manageData;
+    private int txyz;
+    private int desiredListSizeSizeForCalibration;
+
+    float accelerationOffsetValue;
+
+
+    CalibratedIMUFragment calibratedIMUFragment;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spectral_analysis);
+        setContentView(R.layout.activity_calibrating_imu_prompt);
         Log.d(TAG, "onCreate: " + TAG);
 
-        CalibratedIMUFragment calibratedIMUFragment = (CalibratedIMUFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_calibrating_imu);
+        txyz = getResources().getInteger(R.integer.array_index_txyz);
+        desiredListSizeSizeForCalibration = getResources().getInteger(R.integer.device_calibration_array_size);
+
+        calibratedIMUFragment = (CalibratedIMUFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_calibrating_imu);
+
+        manageData = new ManageData();
+        Log.d(TAG, "onCreate: manageData");
 
         connectToDevice();
     }
 
     private void connectToDevice() {
-        bluetoothStream = new BluetoothDataMainStream();
+        bluetoothStream = new BluetoothDeviceManager();
         BluetoothDevice mBluetoothDevice = bluetoothStream.getBluetoothDevice();
 
-        Log.d(TAG, "connectToDevice: Key Time Start");
+        //Log.d(TAG, "connectToDevice: Key Time Start");
 
         boolean isSocketConnected =  bluetoothStream.connectSocket(mBluetoothDevice);
 
+
+        //Run this whole bluetooth initializing thing on a seperate thread, or part of it on a seperate thread
         if (isSocketConnected) {
 
             BluetoothSocket mmSocket = bluetoothStream.getBluetoothSocket();
+            bluetoothStream.getInputStream(mmSocket);
 
-            mmInStream = bluetoothStream.getInputStream(mmSocket);
             Log.d(TAG, "connectToDevice: InputStreamObtained");
             bluetoothStream.start();
         }
-        Log.d(TAG, "connectToDevice: Key Time");
 
         calibrateDevice();
     }
 
-
     private void calibrateDevice() {
-
-        // String st;
-        //        String tmp = "";
-        //
-        //        while ((st = br.readLine()) != null ) {
-        //            tmp +=  st + ";";
-        //        }
-
-  //      String[] rawData = string.split(";;");
-//        ArrayList<float[]> formattedData =  new ArrayList<float[]>() ;
-
-        //for (int i = 0; i < rawData.length; i++) {
-         //   formattedData.add(formatData(rawData[i]));
-            //System.out.println(Arrays.toString(formattedData.get(i)));
-       // }
+        List<String> listRawDeviceData =  BluetoothDeviceData.getList();
 
 
-        ///ArrayList<float[]> formattedData = calibrate.getData(stationaryFile);
+        ArrayList<float[]> formattedDataFromDevice =  manageData.getData(desiredListSizeSizeForCalibration, listRawDeviceData);
+
+        for (int i = 0; i < 38; i++) {
+            Log.d(TAG, "calibrateDevice: " + Arrays.toString(formattedDataFromDevice.get(i)));
+        }
+
+        float[][] accelerometerOffsetData = formattedDataFromDevice.toArray(new float[][]
+                {new float[formattedDataFromDevice.size()]});
+
+
+        float[] getAccelerationFromOneDimension = manageData.getAccelerationFromRawData(accelerometerOffsetData, txyz);
+        float offsetBenchmark = 0.2f;
+        float offsetFailedVal = 100f;
+
+        //offsetValue should be passed to next activity
+        accelerationOffsetValue = manageData.offsetAcceleration(getAccelerationFromOneDimension, offsetBenchmark, offsetFailedVal);
+
+        String calibrationResultMessage;
+        if (accelerationOffsetValue == offsetFailedVal) {
+            calibrationResultMessage = getResources().getString(R.string.calibration_failed_message);
+            Log.d(TAG, "calibrateDevice: " + calibrationResultMessage);
+
+            sendCalibrationMessage(calibrationResultMessage);
+            calibrateDevice();
+        }
+
+        calibrationResultMessage = getResources().getString(R.string.calibration_complete_message);
+        Log.d(TAG, "calibrateDevice: " + calibrationResultMessage);
+        sendCalibrationMessage(calibrationResultMessage);
+
 
     }
 
+    private void sendCalibrationMessage(String message) {
+        calibratedIMUFragment.setCalibratingMessageFeedback(message);
+
+    }
 
 }
--- app/src/main/java/com/smartcpr/junaid/smartcpr/CalibrateIMUFragments/CalibratedIMUFragment.java
+++ app/src/main/java/com/smartcpr/junaid/smartcpr/CalibrateIMUFragments/CalibratedIMUFragment.java
@@ -1,6 +1,5 @@
 package com.smartcpr.junaid.smartcpr.CalibrateIMUFragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,10 +9,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.smartcpr.junaid.smartcpr.R;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class CalibratedIMUFragment extends Fragment {
 
@@ -26,42 +21,15 @@ public class CalibratedIMUFragment extends Fragment {
 
         mCalibratingMessage =  view.findViewById(R.id.calibration_result_feedback);
 
-        ReadRawData(getActivity());
-
         return view;
     }
 
-    public void setCalibratingMessageFeedback(Boolean isDeviceCalibrated){
-        mCalibratingMessage.setText(isDeviceCalibrated.toString());
-        Log.d(TAG, "setDetailsText: isDeviceCalibrated  set");
+    public void setCalibratingMessageFeedback(String deviceCalibrated){
+        mCalibratingMessage.setText(deviceCalibrated);
+
+
+        Log.d(TAG, "setDetailsText: isDeviceCalibrated Message set");
 
     }
 
-    private void ReadRawData(Context context) {
-
-        File path = context.getFilesDir();
-        File file = new File(path, "data.txt");
-
-        while  (true) {
-            try {
-                int length = (int) file.length();
-
-                byte[] bytes = new byte[length];
-
-                FileInputStream in = new FileInputStream(file);
-                try {
-                    in.read(bytes);
-                } finally {
-                    in.close();
-                }
-
-                String contents = new String(bytes);
-
-                Log.d(TAG, "ReadRawData: " + contents);
-            } catch (IOException e) {
-                Log.e(TAG, "File read failed: " + e.toString());
-                Log.d(TAG, "ReadRawData: " + e.getMessage());
-            }
-        }
-    }
 }
