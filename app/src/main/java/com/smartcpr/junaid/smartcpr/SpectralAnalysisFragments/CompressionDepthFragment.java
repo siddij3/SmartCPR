dadd960d884362adc95fdf2b38ddc9b9ee3c52c5
Junaid Siddiqui <junaidas97@gmail.com>
2018-08-31 18:06:25 -0400

    Craeted fragment for UI

---
+++ app/src/main/java/com/smartcpr/junaid/smartcpr/SpectralAnalysisFragments/CompressionDepthFragment.java
@@ -0,0 +1,25 @@
+package com.smartcpr.junaid.smartcpr.SpectralAnalysisFragments;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smartcpr.junaid.smartcpr.R;
+
+public class CompressionDepthFragment extends Fragment{
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_compression_depth, container, false);
+
+
+        return view;
+    }
+
+//Find which rate corresponds to which label
+
+
+    //New function. Change label colours
+
+}
