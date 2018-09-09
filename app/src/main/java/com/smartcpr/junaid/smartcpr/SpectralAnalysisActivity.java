510cf4c017cba70317aa881a4ff81ed324f79714
Junaid Siddiqui <junaidas97@gmail.com>
2018-09-09 13:06:01 -0400

    Depth Feedback Classes UI and activity

--- app/src/main/java/com/smartcpr/junaid/smartcpr/SpectralAnalysisActivity.java
+++ app/src/main/java/com/smartcpr/junaid/smartcpr/SpectralAnalysisActivity.java
@@ -2,12 +2,17 @@ package com.smartcpr.junaid.smartcpr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.smartcpr.junaid.smartcpr.DeviceDetailsFragments.CalibrateButtonFragment;
 import com.smartcpr.junaid.smartcpr.MathOperationsClasses.FastFourierTransform;
 import com.smartcpr.junaid.smartcpr.MathOperationsClasses.SimpleMathOps;
 import com.smartcpr.junaid.smartcpr.ObjectClasses.Complex;
+import com.smartcpr.junaid.smartcpr.SpectralAnalysisFragments.CompressionDepthFragment;
 import com.smartcpr.junaid.smartcpr.SpectralAnalysisFragments.CompressionRateFragment;
 import com.smartcpr.junaid.smartcpr.SpectralAnalysisFragments.SpectralAnalysis;
 
@@ -26,25 +31,29 @@ public class SpectralAnalysisActivity extends AppCompatActivity {
     public static final String EXTRA_VICTIM_MAX_DEPTH
             = "com.smartcpr.junaid.smartcpr.maxvictimdepth";
 
-    private static float offsetAcceleration;
 
     private int txyz;
     private int desiredListSizeForCompression;
+    private float offsetAcceleration;
 
     private static float GRAVITY;
-    private static float OFFSET_BENCHMARK;
-    private static float OFFSET_FAILED_VALUE;
-
 
     private float minVictimDepth;
     private float maxVictimDepth;
 
     Intent intent;
     Bundle bundle;
+    private Handler mHandler;
 
-    private String getOffsetValue() { return Objects.requireNonNull(bundle).getString(EXTRA_OFFSET_ACCELERATION_VALUE); }
+    double lastRateValue;
+    double lastDepthValue;
+
+    CompressionDepthFragment compressionDepthFragment;
+    CompressionRateFragment compressionRateFragment;
+
     private String getMinDepth() { return Objects.requireNonNull(bundle).getString(EXTRA_VICTIM_MIN_DEPTH);  }
     private String getMaxDepth() { return Objects.requireNonNull(bundle).getString(EXTRA_VICTIM_MAX_DEPTH); }
+    private String getOffsetAcceleration() { return Objects.requireNonNull(bundle).getString(EXTRA_OFFSET_ACCELERATION_VALUE); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,47 +63,55 @@ public class SpectralAnalysisActivity extends AppCompatActivity {
         intent = getIntent();
         bundle = intent.getExtras();
 
-        offsetAcceleration = Float.parseFloat(getOffsetValue());
-
-        Log.d(TAG, "onCreate: "  + getMinDepth());
-        Log.d(TAG, "onCreate: " + getMaxDepth());
-
         minVictimDepth = Float.parseFloat(getMinDepth());
         maxVictimDepth = Float.parseFloat(getMaxDepth());
+        offsetAcceleration = Float.parseFloat(getOffsetAcceleration());
 
         txyz = getResources().getInteger(R.integer.array_index_txyz);
         desiredListSizeForCompression = getResources().getInteger(R.integer.compression_list_size);
-
-        OFFSET_BENCHMARK = Float.parseFloat(getResources().getString(R.string.offset_benchmark_value));
-        OFFSET_FAILED_VALUE = Float.parseFloat(getResources().getString(R.string.offset_failed_value));
         GRAVITY = Float.parseFloat(getResources().getString(R.string.gravity_value));
 
+        compressionDepthFragment = (CompressionDepthFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_compression_depth);
+        compressionRateFragment = (CompressionRateFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_compression_rate);
+
+
+        handleMessage();
+        startSpectralAnalysis();
     }
 
+    void handleMessage() {
+        mHandler = new Handler(Looper.getMainLooper()) {
+            public void handleMessage(Message message) {
+                int depthRate = message.what;
+                double  value = (double) message.obj;
+
+                if (depthRate == 0 ) {
+                    Log.d(TAG, "handleMessage: depth: " + value);
+
+                } else if (depthRate == 1) {
+                    Log.d(TAG, "handleMessage: rate: " + value);
+
+                    compressionRateFragment.resetColours(lastRateValue);
+                    compressionRateFragment.changeTextView(value);
+
+                    lastRateValue = value;
+
+                }
+
+            }
+        };
+
+    }
 
     private void startSpectralAnalysis() {
-        SpectralAnalysis spectralAnalysisThread = new SpectralAnalysis(txyz, offsetAcceleration, OFFSET_BENCHMARK, OFFSET_FAILED_VALUE, GRAVITY);
-    }
 
-    private void performSpectralAnalysis(float[] time, float[] acceleration) {
-        int N = time.length;
-
-        //May not need this thing
-        N = FastFourierTransform.setArraySizeExponentOfTwo(N);
-        Log.d(TAG, "performSpectralAnalysis: Array Size " + N);
-
-
-        double hanningAppliedValues[] = SimpleMathOps.applyHanningWindow(acceleration, N);
-
-
-        Complex[] baseComplexArray = FastFourierTransform.baseComplexArrayWithWindow(hanningAppliedValues, N);
-        Complex[] complexArrayFFTValues = FastFourierTransform.simpleFFT(baseComplexArray);
-
-        Complex[] fftPolarSingle = FastFourierTransform.fftDoubleToSingle(complexArrayFFTValues, N, 2);
-        double[] fftSmooth = FastFourierTransform.smoothFFTValues(fftPolarSingle, N);
+        SpectralAnalysis spectralAnalysisThread = new SpectralAnalysis(txyz, GRAVITY, desiredListSizeForCompression, offsetAcceleration, mHandler);
+        new Thread(spectralAnalysisThread).start();
 
 
     }
+
 
 
 }
+
--- app/src/main/java/com/smartcpr/junaid/smartcpr/SpectralAnalysisFragments/CompressionRateFragment.java
+++ app/src/main/java/com/smartcpr/junaid/smartcpr/SpectralAnalysisFragments/CompressionRateFragment.java
@@ -1,4 +1,6 @@
 package com.smartcpr.junaid.smartcpr.SpectralAnalysisFragments;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,19 +17,117 @@ import org.w3c.dom.Text;
 public class CompressionRateFragment extends Fragment{
 
     private final static String TAG = "CompressionRateFragment";
-    private boolean _areLecturesLoaded;
+    View view;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_compression_rate, container, false);
 
+        this.view = view;
 
         return view;
     }
 
 
-    //Find which rate corresponds to which label
+
+    public void changeTextView(double rate) {
+        int roundedRate = (int) (5*(Math.round(rate/5)));
+
+        TextView txtView = findTextView(roundedRate);
+
+        int color = findColour(roundedRate);
+
+        Log.d(TAG, "changeTextView: " +  color);
+
+        txtView.setBackgroundResource(color);
+        txtView.setTextColor(Color.WHITE);
+
+    }
+
+    public void resetColours(double lastRateValue) {
+        int roundedRate = (int) (5*(Math.round(lastRateValue/5)));
+
+        TextView textView = findTextView(roundedRate);
+        int color  = findColour(roundedRate);
+
+        textView.setBackgroundResource(R.color.colorWhite);
+        textView.setTextColor(Color.parseColor(getString(color)));
+
+    }
+
+    private int findColour(int roundedRate) {
+        int color;
+        Log.d(TAG, "findColour: " + roundedRate);
+
+        if (roundedRate <= 120 & roundedRate >= 100 ) {
+            color = R.color.colorGreen;
+        } else if (roundedRate == 130
+                | roundedRate == 125
+                |roundedRate == 95
+                | roundedRate == 90 ) {
+            color = R.color.colorYellow;
+
+        } else  {
+            color = R.color.colorRed;
+        }
+        return color;
+    }
+
+    private TextView findTextView(int roundedRate) {
+        TextView textView;
 
 
-    //New function. Change label colours
+        if (roundedRate >= 140) {
+            textView = view.findViewById(R.id.compression_rate_140);
+
+        } else if (roundedRate == 135) {
+            textView = view.findViewById(R.id.compression_rate_135);
+
+        } else if (roundedRate == 130) {
+            textView = view.findViewById(R.id.compression_rate_130);
+
+        } else if (roundedRate == 125) {
+            textView = view.findViewById(R.id.compression_rate_125);
+
+        } else if (roundedRate == 120) {
+            textView = view.findViewById(R.id.compression_rate_120);
+
+        } else if (roundedRate == 115) {
+            textView = view.findViewById(R.id.compression_rate_115);
+
+        } else if (roundedRate == 110) {
+            textView = view.findViewById(R.id.compression_rate_110);
+
+        } else if (roundedRate == 105) {
+            textView = view.findViewById(R.id.compression_rate_105);
+
+        } else if (roundedRate == 100) {
+            textView = view.findViewById(R.id.compression_rate_100);
+
+        } else if (roundedRate == 95) {
+            textView = view.findViewById(R.id.compression_rate_95);
+
+        } else if (roundedRate == 90) {
+            textView = view.findViewById(R.id.compression_rate_90);
+
+        } else if (roundedRate == 85) {
+            textView = view.findViewById(R.id.compression_rate_85);
+
+        } else if (roundedRate <= 80) {
+            textView = view.findViewById(R.id.compression_rate_80);
+
+        } else  {
+            textView = view.findViewById(R.id.compression_rate_80);
+        }
+
+        Log.d(TAG, "findTextView: " + textView.getText());
+
+
+        return textView;
+
+    }
+
+
+
+
 
 }
