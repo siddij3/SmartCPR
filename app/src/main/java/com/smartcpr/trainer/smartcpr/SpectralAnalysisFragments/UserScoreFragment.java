22e69377ae1fcf357ef2e33a10a973df5f00011b
Junaid Siddiqui <junaidas97@gmail.com>
2018-10-25 09:15:32 -0400

    Fragment with feedback

--- app/src/main/java/com/smartcpr/trainer/smartcpr/SpectralAnalysisFragments/UserScoreFragment.java
+++ app/src/main/java/com/smartcpr/trainer/smartcpr/SpectralAnalysisFragments/UserScoreFragment.java
@@ -2,43 +2,71 @@ package com.smartcpr.trainer.smartcpr.SpectralAnalysisFragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.smartcpr.junaid.smartcpr.R;
 
 public class UserScoreFragment extends Fragment {
-    private void getExistingScore(String filePath, float absRate, float absDepth) {
-        float[] rates = new float[];
-        float[] depths = new float[];
 
+    private final static String TAG = "UserScoreFragment";
 
-    }
+    private TextView mPerformanceFeedback;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_feedback, container, false);
 
+        mPerformanceFeedback = view.findViewById(R.id.user_feedback);
 
         return view;
     }
+
+    public void setFeedbackMessage(String msg) {
+        mPerformanceFeedback.setText(msg);
+
+        Log.d(TAG, "setFeedbackMessage: ");
+    }
+
+    //0th index is depth. 1st index is rate
+    public void compareScores(float[] userScores, double[] newScores) {
+        String msgDepth;
+        String msgRate;
+
+
+        double oldDepthScore = (double) userScores[0];
+        double oldRateScore = (double)  userScores[1];
+
+        double newDepthScore = newScores[0];
+        double newRateScore = newScores[1];
+
+
+        //Smaller score is better
+        if (newDepthScore <oldDepthScore) {
+            msgDepth = "Better Depth";
+        } else if (newDepthScore > oldDepthScore) {
+            msgDepth = "Worse Depth ";
+        } else {
+            msgDepth = "About the Same Depth";
+        }
+
+        if (newRateScore < oldRateScore) {
+            msgRate = "Better Rate ";
+
+        } else if (newRateScore > oldRateScore) {
+            msgRate = "Worse Rate ";
+
+        } else {
+            msgRate = "About the Same Rate";
+
+        }
+
+        String msg = msgDepth + ", " + msgRate;
+
+        setFeedbackMessage(msg);
+
+    }
 }
-
-/*
-
-
-def compareScore(currentScore, previousScore):
-    msgDepth = ""
-    msgRate = ""
-    if not currentScore or not previousScore:
-        return "", ""
-
-    if currentScore[0] < previousScore[0]:
-        msgDepth = "Better Depth Than Your Previous Attempt"
-    elif currentScore[0] > previousScore[0]:
-        msgDepth = "Worse Depth Than Your Previous Attempt"
-    else:
-        msgDepth = "About the Same as Your Previous Attempt"
-
-    return msgDepth, msgRate
-    */
